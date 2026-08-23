517abfd549c12e032bdcd1a6f3d0185849725be2
Coder-kanji95 <kavijasaluwadana@gmail.com>
2026-08-23 16:34:51 +0100

    added code for bringing rook sac info of one game together & getting next game & previous game navigation working -finally

--- matplolib_chessboard.py
+++ matplolib_chessboard.py
@@ -22,23 +22,30 @@ def addLabels(ax):
         ax.text(i + 0.5, -0.3, chr(65 + i), ha = "center", va = "center", fontname = "Courier New", fontsize = 16, fontweight = "bold")
         ax.text(-0.3, i + 0.5, str(8 - i), ha = "center", va = "center", fontname = "Courier New", fontsize = 16, fontweight = "bold")
 
-# def addPieces(ax, pieces, fen='rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR'):
-#     fen = fen.split("/")
-    
-#     for i, row in enumerate(fen):
-#         col = 0
-#         for char in row:
-#             if char.isdigit():
-#                 col = col + int(char)
-#             else:
-#                 ax.text(col + 0.5, i + 0.56, pieces[char], fontsize = 40, ha = "center", va = "center")
-#                 col = col + 1
+def goThruGame(game, fig, ax, columnMap, pieces, previousBtn, nextBtn, pieceTexts):
+    gameInfo = game[0]
+    #index = rookSac[1]
+    #materialGained = rookSac[2]
+
+    pgn = gameInfo["pgn"]
+    chessGame = chess.pgn.read_game(io.StringIO(pgn)) 
+
+    board = chessGame.board()
+    moves = list(chessGame.mainline_moves())
+
+    currentMove = [0]
+
+    #update board
+    updateBoard(fig, ax, pieceTexts, board, columnMap, pieces)
+
+    previousBtn.on_clicked(lambda event: previousTurn(fig, ax, pieceTexts, board, columnMap, pieces, currentMove))
+    nextBtn.on_clicked(lambda event: nextTurn(fig, ax, pieceTexts, board, columnMap, pieces, moves, currentMove))
 
 def updateBoard(fig, ax, pieceTexts, board, columnMap, pieces):
     #remove old piece Text objects (avoid overlap drawing)
     for pieceText in pieceTexts:
         pieceText.remove()
-    
+
     pieceTexts.clear()
 
     #draw current position
@@ -75,9 +82,52 @@ def nextTurn(fig, ax, pieceTexts, board, columnMap, pieces, moves, currentMove):
 
         updateBoard(fig, ax, pieceTexts, board, columnMap, pieces)
 
+def previousGame(rookSacGames, currentGame, fig, ax, columnMap, pieces, previousBtn, nextBtn, pieceTexts):
+    if currentGame[0] > 0:
+        currentGame[0] = currentGame[0] - 1
+
+        goThruGame(rookSacGames[currentGame[0]], fig, ax, columnMap, pieces, previousBtn, nextBtn, pieceTexts)
+
+def nextGame(rookSacGames, currentGame, fig, ax, columnMap, pieces, previousBtn, nextBtn, pieceTexts):
+    if currentGame[0] < len(rookSacGames)-1: #go to the next game if not already at the last game
+        currentGame[0] = currentGame[0] + 1
+
+        goThruGame(rookSacGames[currentGame[0]], fig, ax, columnMap, pieces, previousBtn, nextBtn, pieceTexts)
 
 #This is called by app.py (main program file) to display rook sacs. Inside of startDisplay(), it runs the other subroutines as needed
 def startDisplay(rookSacs):
+    #bringing the rook sac info of each game together
+    #structure of rookSacGames list - #[index, materialGained] is the info needed for one rook sac
+    #[
+    #   [game1,  [ [index, materialGained], [index, materialGained] ] ....],
+    #   [game2, [ [index, materialGained], [index, materialGained] ] ....],
+    #   ......
+    #]
+    rookSacGames = []
+    for i in range(0, len(rookSacs)-1):
+        game = []
+        info = []
+        sacInfo = []
+        nextSacInfo = []
+
+        #check if the current rook sac game & next one are the same
+        #if they are, merge the rook sac infos together
+        rookSac = rookSacs[i]
+        if rookSac[0] == rookSacs[i+1][0]: #the first item (index 0) => the game info dictionary
+            game.append(rookSac[0])
+
+            sacInfo.append(rookSac[1]) #the next item is the index of the rook sac as it was found in the main program file (index+1 is the move number where thr rook sac happens)
+            sacInfo.append(rookSac[2]) #rookSac[2] is the material recovered by the player who sac'd the rook
+
+            nextSacInfo.append(rookSacs[i+1][1]) #these are the same as rookSac[1] & rookSac[2] but its from the next inner list in rookSacs list
+            nextSacInfo.append(rookSacs[i+1][2])
+
+            info.append(sacInfo)
+            info.append(nextSacInfo)
+
+            game.append(info)
+            rookSacGames.append(game)
+
     columnMap = {"a": 0.5, "b": 1.5, "c": 2.5, "d": 3.5, "e": 4.5, "f": 5.5, "g": 6.5, "h": 7.5}
 
     pieces = {
@@ -85,119 +135,41 @@ def startDisplay(rookSacs):
     'k': '♚', 'q': '♛', 'r': '♜', 'b': '♝', 'n': '♞', 'p': '♟'
     }
 
-    boardState = {
-    "a1": "R",
-    "a2": "P",
-    "a3": "",
-    "a4": "",
-    "a5": "",
-    "a6": "",
-    "a7": "p",
-    "a8": "r",
-    
-    "b1": "N",
-    "b2": "P",
-    "b3": "",
-    "b4": "",
-    "b5": "",
-    "b6": "",
-    "b7": "p",
-    "b8": "n",
-    
-    "c1": "B",
-    "c2": "P",
-    "c3": "",
-    "c4": "",
-    "c5": "",
-    "c6": "",
-    "c7": "p",
-    "c8": "b",
-    
-    "d1": "Q",
-    "d2": "P",
-    "d3": "",
-    "d4": "",
-    "d5": "",
-    "d6": "",
-    "d7": "p",
-    "d8": "q",
-    
-    "e1": "K",
-    "e2": "P",
-    "e3": "",
-    "e4": "",
-    "e5": "",
-    "e6": "",
-    "e7": "p",
-    "e8": "k",
-    
-    "f1": "B",
-    "f2": "P",
-    "f3": "",
-    "f4": "",
-    "f5": "",
-    "f6": "",
-    "f7": "p",
-    "f8": "b",
-    
-    "g1": "N",
-    "g2": "P",
-    "g3": "",
-    "g4": "",
-    "g5": "",
-    "g6": "",
-    "g7": "p",
-    "g8": "n",
-    
-    "h1": "R",
-    "h2": "P",
-    "h3": "",
-    "h4": "",
-    "h5": "",
-    "h6": "",
-    "h7": "p",
-    "h8": "r",
-    }
-
     chessBoard = createBoard()
     fig, ax = plt.subplots(figsize = (10, 10))
     plt.imshow(chessBoard, extent = (0, 8, 8, 0), origin = "lower")
 
-    # addPieces(ax, pieces)
     addLabels(ax)
 
     ax.set_xlim(-0.7, 8.7)
     ax.set_ylim(8.7, -0.7)
 
     ax.set_title("Rook Sacrifice Games", fontsize=20, pad=20)
+
+    #buttons to go through moves
+    #plt.axes([left, bottom, width, height])
+    previousMove = plt.axes([0.36, 0.02, 0.1, 0.05])
+    nextMove = plt.axes([0.57, 0.02, 0.1, 0.05])
+
+    previousBtn = Button(previousMove, "◀")
+    nextBtn = Button(nextMove, "▶")
+
+    #buttons to go to next & previous game
+    prevGame = plt.axes([0.15, 0.02, 0.1, 0.05])
+    nxtGame = plt.axes([0.76, 0.02, 0.1, 0.05])
+
+    previousGameBtn = Button(prevGame, "Previous Game")
+    nextGameBtn = Button(nxtGame, "Next Game")
+
+    pieceTexts = []
+    #start with the first game
+    game = rookSacGames[0]
+    goThruGame(game, fig, ax, columnMap, pieces, previousBtn, nextBtn, pieceTexts)
+
+    currentGame = [0]
+    nextGameBtn.on_clicked(lambda event: nextGame(rookSacGames, currentGame, fig, ax, columnMap, pieces, previousBtn, nextBtn, pieceTexts))
+
+    previousGameBtn.on_clicked(lambda event: previousGame(rookSacGames, currentGame, fig, ax, columnMap, pieces, previousBtn, nextBtn, pieceTexts))
     
-    for rookSac in rookSacs:
-        game = rookSac[0]
-        index = rookSac[1]
-        materialGained = rookSac[2]
-
-        pgn = game["pgn"]
-        game = chess.pgn.read_game(io.StringIO(pgn)) 
-
-        board = game.board()
-        moves = list(game.mainline_moves())
-
-        currentMove = [0]
-
-        pieceTexts = []
-
-        #update board
-        updateBoard(fig, ax, pieceTexts, board, columnMap, pieces)
-
-        #buttons to go through moves
-        previous = plt.axes([0.35, 0.02, 0.1, 0.05])
-        next = plt.axes([0.55, 0.02, 0.1, 0.05])
-    
-        previousBtn = Button(previous, "◀")
-        nextBtn = Button(next, "▶")
-    
-        previousBtn.on_clicked(lambda event: previousTurn(fig, ax, pieceTexts, board, columnMap, pieces, currentMove))
-        nextBtn.on_clicked(lambda event: nextTurn(fig, ax, pieceTexts, board, columnMap, pieces, moves, currentMove))
-
     ax.axis("off")
     plt.show()
